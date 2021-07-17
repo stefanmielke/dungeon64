4f13ea55f887e9c425721371bbab086a15603b6a
Stefan Mielke <stefanmielke@fastmail.com>
2021-07-17 19:05:14 -0300

    Show correct colors for players when attacking.

--- src/combat/party.c
+++ src/combat/party.c
@@ -37,21 +37,21 @@ PartyMember get_start_member_for_class(Class class, const char *name) {
 }
 
 void party_render(Party *party, Gfx **glistp, Dynamic *dynamicp, s8 highlight_index) {
-	FONTCOLM(FONT_COL);
-
 	char text[100];
 	for (s8 i = 0; i < party->current_member_count; ++i) {
 		const int start_x = 20 + (i * 80);
 		PartyMember *member = &party->members[i];
 
-		if (highlight_index >= 0 && member->current_health > 0) {
+		if (member->current_health <= 0) {
+			FONTCOLM(FONT_COL_GREY);
+		} else if (highlight_index >= 0) {
 			if (highlight_index == i) {
 				FONTCOLM(FONT_COL);
 			} else {
 				FONTCOLM(FONT_COL_GREY);
 			}
-		} else if (member->current_health <= 0) {
-			FONTCOLM(FONT_COL_GREY);
+		} else {
+			FONTCOLM(FONT_COL);
 		}
 
 		sprintf(text, "%s", member->name);
