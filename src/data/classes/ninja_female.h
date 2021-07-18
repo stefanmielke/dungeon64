179d36e8322b198e09092385134c15ade7542ec7
Stefan Mielke <stefanmielke@fastmail.com>
2021-07-18 16:57:10 -0300

    Adding combat other gender animations for all classes.

---
+++ src/data/classes/ninja_female.h
@@ -0,0 +1,107 @@
+#pragma once
+
+#include <ultra64.h>
+
+/* IDLE */
+
+static Gfx spr_ninja_female_idle1_dummy_aligner[] = {gsSPEndDisplayList()};
+unsigned short spr_ninja_female_idle1[] = {
+#include "../../res/gfx/player/ninja_female_idle1.gen.h"
+};
+
+static Gfx spr_ninja_female_idle2_dummy_aligner[] = {gsSPEndDisplayList()};
+unsigned short spr_ninja_female_idle2[] = {
+#include "../../res/gfx/player/ninja_female_idle2.gen.h"
+};
+
+static Gfx spr_ninja_female_idle3_dummy_aligner[] = {gsSPEndDisplayList()};
+unsigned short spr_ninja_female_idle3[] = {
+#include "../../res/gfx/player/ninja_female_idle3.gen.h"
+};
+
+/* ATTACK 1 */
+
+static Gfx spr_ninja_female_attack_1_1_dummy_aligner[] = {gsSPEndDisplayList()};
+unsigned short spr_ninja_female_attack_1_1[] = {
+#include "../../res/gfx/player/ninja_female_attack_1_1.gen.h"
+};
+
+static Gfx spr_ninja_female_attack_1_2_dummy_aligner[] = {gsSPEndDisplayList()};
+unsigned short spr_ninja_female_attack_1_2[] = {
+#include "../../res/gfx/player/ninja_female_attack_1_2.gen.h"
+};
+
+static Gfx spr_ninja_female_attack_1_3_dummy_aligner[] = {gsSPEndDisplayList()};
+unsigned short spr_ninja_female_attack_1_3[] = {
+#include "../../res/gfx/player/ninja_female_attack_1_3.gen.h"
+};
+
+/* ATTACK 2 */
+
+static Gfx spr_ninja_female_attack_2_1_dummy_aligner[] = {gsSPEndDisplayList()};
+unsigned short spr_ninja_female_attack_2_1[] = {
+#include "../../res/gfx/player/ninja_female_attack_2_1.gen.h"
+};
+
+static Gfx spr_ninja_female_attack_2_2_dummy_aligner[] = {gsSPEndDisplayList()};
+unsigned short spr_ninja_female_attack_2_2[] = {
+#include "../../res/gfx/player/ninja_female_attack_2_2.gen.h"
+};
+
+static Gfx spr_ninja_female_attack_2_3_dummy_aligner[] = {gsSPEndDisplayList()};
+unsigned short spr_ninja_female_attack_2_3[] = {
+#include "../../res/gfx/player/ninja_female_attack_2_3.gen.h"
+};
+
+/* ATTACK 3 */
+
+static Gfx spr_ninja_female_attack_3_1_dummy_aligner[] = {gsSPEndDisplayList()};
+unsigned short spr_ninja_female_attack_3_1[] = {
+#include "../../res/gfx/player/ninja_female_attack_3_1.gen.h"
+};
+
+static Gfx spr_ninja_female_attack_3_2_dummy_aligner[] = {gsSPEndDisplayList()};
+unsigned short spr_ninja_female_attack_3_2[] = {
+#include "../../res/gfx/player/ninja_female_attack_3_2.gen.h"
+};
+
+static Gfx spr_ninja_female_attack_3_3_dummy_aligner[] = {gsSPEndDisplayList()};
+unsigned short spr_ninja_female_attack_3_3[] = {
+#include "../../res/gfx/player/ninja_female_attack_3_3.gen.h"
+};
+
+/* HIT */
+
+static Gfx spr_ninja_female_hit1_dummy_aligner[] = {gsSPEndDisplayList()};
+unsigned short spr_ninja_female_hit1[] = {
+#include "../../res/gfx/player/ninja_female_hit1.gen.h"
+};
+
+/* DEAD */
+
+static Gfx spr_ninja_female_dead1_dummy_aligner[] = {gsSPEndDisplayList()};
+unsigned short spr_ninja_female_dead1[] = {
+#include "../../res/gfx/player/ninja_female_dead1.gen.h"
+};
+
+/* WIN */
+
+static Gfx spr_ninja_female_win1_dummy_aligner[] = {gsSPEndDisplayList()};
+unsigned short spr_ninja_female_win1[] = {
+#include "../../res/gfx/player/ninja_female_win1.gen.h"
+};
+
+static Gfx spr_ninja_female_win2_dummy_aligner[] = {gsSPEndDisplayList()};
+unsigned short spr_ninja_female_win2[] = {
+#include "../../res/gfx/player/ninja_female_win2.gen.h"
+};
+
+static Gfx spr_ninja_female_win3_dummy_aligner[] = {gsSPEndDisplayList()};
+unsigned short spr_ninja_female_win3[] = {
+#include "../../res/gfx/player/ninja_female_win3.gen.h"
+};
+
+static Gfx spr_ninja_female_win4_dummy_aligner[] = {gsSPEndDisplayList()};
+unsigned short spr_ninja_female_win4[] = {
+#include "../../res/gfx/player/ninja_female_win4.gen.h"
+};
---
+++ src/data/classes/wizard_male.h
@@ -0,0 +1,107 @@
+#pragma once
+
+#include <ultra64.h>
+
+/* IDLE */
+
+static Gfx spr_wizard_male_idle1_dummy_aligner[] = {gsSPEndDisplayList()};
+unsigned short spr_wizard_male_idle1[] = {
+#include "../../res/gfx/player/wizard_male_idle1.gen.h"
+};
+
+static Gfx spr_wizard_male_idle2_dummy_aligner[] = {gsSPEndDisplayList()};
+unsigned short spr_wizard_male_idle2[] = {
+#include "../../res/gfx/player/wizard_male_idle2.gen.h"
+};
+
+static Gfx spr_wizard_male_idle3_dummy_aligner[] = {gsSPEndDisplayList()};
+unsigned short spr_wizard_male_idle3[] = {
+#include "../../res/gfx/player/wizard_male_idle3.gen.h"
+};
+
+/* ATTACK 1 */
+
+static Gfx spr_wizard_male_attack_1_1_dummy_aligner[] = {gsSPEndDisplayList()};
+unsigned short spr_wizard_male_attack_1_1[] = {
+#include "../../res/gfx/player/wizard_male_attack_1_1.gen.h"
+};
+
+static Gfx spr_wizard_male_attack_1_2_dummy_aligner[] = {gsSPEndDisplayList()};
+unsigned short spr_wizard_male_attack_1_2[] = {
+#include "../../res/gfx/player/wizard_male_attack_1_2.gen.h"
+};
+
+static Gfx spr_wizard_male_attack_1_3_dummy_aligner[] = {gsSPEndDisplayList()};
+unsigned short spr_wizard_male_attack_1_3[] = {
+#include "../../res/gfx/player/wizard_male_attack_1_3.gen.h"
+};
+
+/* ATTACK 2 */
+
+static Gfx spr_wizard_male_attack_2_1_dummy_aligner[] = {gsSPEndDisplayList()};
+unsigned short spr_wizard_male_attack_2_1[] = {
+#include "../../res/gfx/player/wizard_male_attack_2_1.gen.h"
+};
+
+static Gfx spr_wizard_male_attack_2_2_dummy_aligner[] = {gsSPEndDisplayList()};
+unsigned short spr_wizard_male_attack_2_2[] = {
+#include "../../res/gfx/player/wizard_male_attack_2_2.gen.h"
+};
+
+static Gfx spr_wizard_male_attack_2_3_dummy_aligner[] = {gsSPEndDisplayList()};
+unsigned short spr_wizard_male_attack_2_3[] = {
+#include "../../res/gfx/player/wizard_male_attack_2_3.gen.h"
+};
+
+/* ATTACK 3 */
+
+static Gfx spr_wizard_male_attack_3_1_dummy_aligner[] = {gsSPEndDisplayList()};
+unsigned short spr_wizard_male_attack_3_1[] = {
+#include "../../res/gfx/player/wizard_male_attack_3_1.gen.h"
+};
+
+static Gfx spr_wizard_male_attack_3_2_dummy_aligner[] = {gsSPEndDisplayList()};
+unsigned short spr_wizard_male_attack_3_2[] = {
+#include "../../res/gfx/player/wizard_male_attack_3_2.gen.h"
+};
+
+static Gfx spr_wizard_male_attack_3_3_dummy_aligner[] = {gsSPEndDisplayList()};
+unsigned short spr_wizard_male_attack_3_3[] = {
+#include "../../res/gfx/player/wizard_male_attack_3_3.gen.h"
+};
+
+/* HIT */
+
+static Gfx spr_wizard_male_hit1_dummy_aligner[] = {gsSPEndDisplayList()};
+unsigned short spr_wizard_male_hit1[] = {
+#include "../../res/gfx/player/wizard_male_hit1.gen.h"
+};
+
+/* DEAD */
+
+static Gfx spr_wizard_male_dead1_dummy_aligner[] = {gsSPEndDisplayList()};
+unsigned short spr_wizard_male_dead1[] = {
+#include "../../res/gfx/player/wizard_male_dead1.gen.h"
+};
+
+/* WIN */
+
+static Gfx spr_wizard_male_win1_dummy_aligner[] = {gsSPEndDisplayList()};
+unsigned short spr_wizard_male_win1[] = {
+#include "../../res/gfx/player/wizard_male_win1.gen.h"
+};
+
+static Gfx spr_wizard_male_win2_dummy_aligner[] = {gsSPEndDisplayList()};
+unsigned short spr_wizard_male_win2[] = {
+#include "../../res/gfx/player/wizard_male_win2.gen.h"
+};
+
+static Gfx spr_wizard_male_win3_dummy_aligner[] = {gsSPEndDisplayList()};
+unsigned short spr_wizard_male_win3[] = {
+#include "../../res/gfx/player/wizard_male_win3.gen.h"
+};
+
+static Gfx spr_wizard_male_win4_dummy_aligner[] = {gsSPEndDisplayList()};
+unsigned short spr_wizard_male_win4[] = {
+#include "../../res/gfx/player/wizard_male_win4.gen.h"
+};
